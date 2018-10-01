1d2411c3dddbd40f0b1d73a96b9595b5f7808073
Steven <s.zielinski72@gmail.com>
2018-10-01 13:36:52 -0400

    Added memory profiler to the program. Moved settings to the top of the program. Changed needed setting to specify a single number. Have the program now out put the result of the addition and the 2 numbers generated. Fixed the math behind the addition as floating point division was creating odd numbers. Changed the iteration to run off a setting.

--- assignment1.py
+++ assignment1.py
@@ -1,6 +1,18 @@
 import random
 from timeit import default_timer as timer
+from memory_profiler import profile
 import matplotlib.pyplot as plt
+
+# This specifies the size of the randomly generated large numbers
+needed = 4
+# This should be the name of the file that you would like the program to
+# generate that will have the performance data
+plotFile = 'performance.png'
+# A value of 0 will give a plot based on runtime while a value of 1 will give
+# a plot of the memory usage
+memoryOrTime = 0;
+# The number of times the program should add numbers of length needed
+numberOfIterations = 1;
 
 def add(a, b):
     a = a[::-1]
@@ -12,7 +24,7 @@ def add(a, b):
         bb = 0 if len(b)<=i else int(b[i])
         result = aa + bb + carry
         realresult = result%10
-        carry = result/10
+        carry = result//10
         numbers.append(realresult)
     if carry != 0:
         numbers.append(carry)
@@ -26,20 +38,22 @@ def generate(n):
     
 length = []
 time = []
-needed = [4,8,16,32,64,128,256,512]
-for i in needed:
-    print(i)
-    numerator = 0
-    denominator = 0
-    for iteration in range(0,1000):
-        x = generate(i)
-        y = generate(i)
-        start = timer()
-        add(x,y)
-        end = timer()
-        numerator = numerator + end-start
-        denominator = denominator+1
-    length.append(i)
-    time.append(numerator/denominator)
+
+print('size of input numbers', needed)
+numerator = 0
+denominator = 0
+for iteration in range(0,numberOfIterations):
+    x = generate(needed)
+    y = generate(needed)
+    start = timer()
+    result = add(x,y)
+    end = timer()
+    print('x = ', x)
+    print('y = ', y)
+    print('x + y = ',  result)
+    numerator = numerator + end-start
+    denominator = denominator+1
+length.append(needed)
+time.append(numerator/denominator)
 plt.plot(length,time)
-plt.savefig('performance.png')
+plt.savefig(plotFile)
