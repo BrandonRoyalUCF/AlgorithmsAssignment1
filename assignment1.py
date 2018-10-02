2f427385d93132c3b0f208382884a8a9f948e4c0
StevenZielinski <s.zielinski72@gmail.com>
2018-10-01 20:22:27 -0400

    Added in Brandon's memory profiler.

--- assignment1.py
+++ assignment1.py
@@ -1,18 +1,20 @@
 import random
 from timeit import default_timer as timer
-from memory_profiler import profile
+from memory_profiler import memory_usage
 import matplotlib.pyplot as plt
 
 # This specifies the size of the randomly generated large numbers
 needed = 4
 # This should be the name of the file that you would like the program to
 # generate that will have the performance data
-plotFile = 'performance.png'
-# A value of 0 will give a plot based on runtime while a value of 1 will give
-# a plot of the memory usage
-memoryOrTime = 0;
+plotFile1 = 'Nof4Time.png'
+plotFile2 = 'Nof4Memory.png'
 # The number of times the program should add numbers of length needed
-numberOfIterations = 1000;
+numberOfIterations = 1000
+# Title of the graph of time per run
+graphTitle1 = 'N of 4 Time'
+graphTitle2 = 'N of 4 Memory'
+
 
 def add(a, b):
     """Adds two numbers that are represented as Strings
@@ -45,6 +47,7 @@ def add(a, b):
     answer = ''.join([str(digit) for digit in numbers])
     return answer
 
+
 def generate(n):
     """Generates a random number of size n.
 
@@ -60,30 +63,66 @@ def generate(n):
 
     answer = str(random.randint(1,9))+ ''.join([str(random.randint(0,9)) for _ in range(1,n)])
     return answer
-    
-length = []
-time = []
 
-print('size of input numbers', needed)
-numerator = 0
-denominator = 0
-maxHeight = 0.0
-for iteration in range(0,numberOfIterations):
-    x = generate(needed)
-    y = generate(needed)
-    start = timer()
-    result = add(x,y)
-    end = timer()
-    print('x = ', x)
-    print('y = ', y)
-    print('x + y = ',  result)
-    time.append(end-start)
-    maxHeight = max(maxHeight, end-start)
-    numerator = numerator + end-start
-    denominator = denominator+1
-text = 'Average = ', numerator/denominator
-plt.xlabel('Runs')
-plt.ylabel('Time')
-plt.plot(range(0, numberOfIterations), time)
-plt.text(0, maxHeight*0.98, text)
-plt.savefig(plotFile)
+
+def memory_test(n):
+    """Test method to be used by the memory profile.
+
+    Test method that randomly generates two numbers of size n and then adds them.
+    This method was specifically created to analyze memory usage with memory_profiler.
+
+    Args:
+        n: The number of digits each random number will have.
+
+    Returns:
+        result: The sum of the two random numbers.
+    """
+    x = generate(n)
+    y = generate(n)
+    result = add(x, y)
+    return result
+
+
+if __name__ == '__main__':
+    length = []
+    time = []
+
+    print('size of input numbers', needed)
+    numerator = 0
+    denominator = 0
+    maxHeight = 0.0
+    maxMemory = 0.0
+    for iteration in range(0,numberOfIterations):
+        x = generate(needed)
+        y = generate(needed)
+        start = timer()
+        result = add(x,y)
+        end = timer()
+        print('x = ', x)
+        print('y = ', y)
+        print('x + y = ',  result)
+        memory_points = memory_usage((memory_test, (needed,),), interval=0.00000001)
+        memorySum = sum(memory_points)
+        avgMemory = memorySum / numberOfIterations
+        time.append(end-start)
+        maxHeight = max(maxHeight, end-start)
+        numerator = numerator + end-start
+        denominator = denominator+1
+    for j in memory_points:
+        maxMemory = max(maxMemory, j)
+    text = 'Average = ', numerator/denominator
+    memText = 'Average = ', avgMemory
+    plt.figure(0)
+    plt.xlabel('Runs')
+    plt.ylabel('Time')
+    plt.title(graphTitle1)
+    plt.plot(range(0, numberOfIterations), time)
+    plt.text(0, maxHeight*0.98, text)
+    plt.savefig(plotFile1)
+    plt.figure(1)
+    plt.xlabel('Runs')
+    plt.ylabel('Memory Usage')
+    plt.title(graphTitle2)
+    plt.plot(range(0, numberOfIterations), memory_points)
+    plt.text(0, maxMemory*0.98, memText)
+    plt.savefig(plotFile2)
